4d78d83c7d40295112bf6d8bb19280c161a931f9
Sampo15 <75014754+Sampo15@users.noreply.github.com>
2021-01-21 18:46:11 +0200

    lol

---
+++ script.js
@@ -0,0 +1,19 @@
+(function(){
+document.addEventListener("mousemove, parallax");
+const elem = document.querySelector("#parallax");
+
+function parallax(e){
+    let w = window.innerWidth / 2;
+    let h = window.innerHeight / 2;
+    let mouseX = e.clientX;
+    let mouseY = e.clientY;
+    let rentoudu =  `${50-(mouseX-w)*0.01}% ${50-(mouseY-h)*0.01}% `;
+    let seapic =  `${50-(mouseX-w)*0.02}% ${50-(mouseY-h)*0.02}% `;
+    let catto =  `${50-(mouseX-w)*0.02}% ${50-(mouseY-h)*0.02}% `;
+    let x =  `${rentoudu},${seapic},${catto}`;
+    console.log(x);
+    elem.style.backgroundPosition = x;
+
+}
+
+})();
