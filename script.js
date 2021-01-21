e59b95188967cdd8c6cb63837b5cc4de9abd5b46
Sampo15 <75014754+Sampo15@users.noreply.github.com>
2021-01-21 18:47:41 +0200

    sad

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ function parallax(e){
     let mouseY = e.clientY;
     let rentoudu =  `${50-(mouseX-w)*0.01}% ${50-(mouseY-h)*0.01}% `;
     let seapic =  `${50-(mouseX-w)*0.02}% ${50-(mouseY-h)*0.02}% `;
-    let catto =  `${50-(mouseX-w)*0.02}% ${50-(mouseY-h)*0.02}% `;
-    let x =  `${rentoudu},${seapic},${catto}`;
+   
+    let x =  `${rentoudu},${seapic}`;
     console.log(x);
     elem.style.backgroundPosition = x;
 
