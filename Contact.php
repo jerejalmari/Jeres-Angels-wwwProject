af2e3a7be7634efb51038cc2985713aca6c1e6f4
Sampo15 <75014754+Sampo15@users.noreply.github.com>
2021-02-02 09:52:09 +0200

    Jotaki

--- Contact.php
+++ Contact.php
@@ -3,17 +3,18 @@ $name = $_REQUEST['name'];
 $email = $_REQUEST['email'];
 $message = $_REQUEST['message'];
 
+
+$myUrl = "https://htmlprojekti.000webhostapp.com/#"
+
 if (empty($name) || empty($email) || empty($message))
   {
-      echo "Please fill all the fields";
+      echo ('Please fill all the fields');
   }  
 else
 {
-    mail("jeres.angels123@gmail.com", "Lomailu Message", $message , "From: $name  
-    <$email>" );
-    echo "<script type='text/javascript'>alert('Your message was sent
-     succsefully');
-    window.history.log(-1);
+    mail("jeres.angels123@gmail.com", "Lomailu Message", $message , "From: $name  <$email>" );
+    echo "<script type='text/javascript'>alert('Your message was sent succsefully!');
+    window.location.href = '$myUrl';
     </script>";
 }
 ?>
